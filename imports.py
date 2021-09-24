fa52e978035860c7f2d3af14eaf0a2935fa9fe35
BeatrizTSilva <60941112+BeatrizTSilva@users.noreply.github.com>
2021-09-24 10:56:25 +0000

    Delete imports.py

--- imports.py
+++
@@ -1,8 +0,0 @@
-# https://stackoverflow.com/questions/28867795/reading-i2c-data-from-gps
-import smbus
-import time
-
-bus = smbus.SMBus(1)
-address = 0x42
-bus.read_byte_data(address,0x00)
-bus.read_byte(address)
